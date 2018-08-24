e4b23ab34582d66b4be2d069d2abd87aa951f30b
Kumaresan <kumaresan.ramu@gmail.com>
2018-08-24 11:16:39 +0530

    Create ClickjackingPreventionFilter.java

---
+++ ClickjackingPreventionFilter.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+ 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+ 
+/* types of X-Frame-Options
+   X-Frame-Options: deny
+   X-Frame-Options: sameorigin
+   X-Frame-Options: allow-from https://example.com/ */
+ 
+public class ClickjackingPreventionFilter implements Filter 
+{
+  private String mode = "DENY";
+   
+// Add X-FRAME-OPTIONS response header to tell any other browsers who   not to display this //content in a frame.
+     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+         HttpServletResponse res = (HttpServletResponse)response;
+         res.addHeader("X-FRAME-OPTIONS", mode );   
+         chain.doFilter(request, response);
+     }
+     public void destroy() {
+     }
+      
+     public void init(FilterConfig filterConfig) {
+         String configMode = filterConfig.getInitParameter("mode");
+         if ( configMode != null ) {
+             mode = configMode;
+         }
+     }
+}
